ad6a269e2d4912dbee6b6e6aa49ebb490e87f92c
Derek <harrison.derek.w@gmail.com>
2021-10-07 14:41:31 +0200

    refactor

---
+++ fib_heap.hpp
@@ -0,0 +1,15 @@
+/*
+ * fib_heap.hpp
+ *
+ *  Created on: Oct 7, 2021
+ *      Author: d-w-h
+ */
+
+#ifndef FIB_HEAP_HPP_
+#define FIB_HEAP_HPP_
+
+#include <vector>
+
+std::vector<int> shortest_reach(int n, std::vector< std::vector<int> >& edges, int s);
+
+#endif /* FIB_HEAP_HPP_ */
